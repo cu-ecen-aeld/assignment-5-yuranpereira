e8ff27dd0046a8af7cedf3bc3514c339dfb71ac2
Yuran Pereira <yuran.pereira@hotmail.com>
2023-06-19 02:44:38 +0530

    Assignment 4

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '21b7c6c8c9ac70f1c9d849dd726349f3def0366a'
+AESD_ASSIGNMENTS_VERSION = 21b7c6c8c9ac70f1c9d849dd726349f3def0366a
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,8 +15,6 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 FINDER_APP_ROOT=$(@D)/finder-app
 
-# $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-#$(MAKE)	-C $(@D)/finder-app clean
 #$(MAKE)	CROSS_COMPILE=$(TARGET_CC) -C $(@D)/finder-app all
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(TARGET_CC) $(FINDER_APP_ROOT)/writer.c -o $(FINDER_APP_ROOT)/writer
